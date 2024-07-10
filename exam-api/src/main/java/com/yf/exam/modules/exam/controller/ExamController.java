b739064892a8adc894dd6afaf019ed07072b3628
云帆互联 <xiaoquan328@163.com>
2024-07-10 15:33:32 +0800

    更新代码到V2.0版本
    修复一些安全漏洞

--- exam-api/src/main/java/com/yf/exam/modules/exam/controller/ExamController.java
+++ exam-api/src/main/java/com/yf/exam/modules/exam/controller/ExamController.java
@@ -16,6 +16,7 @@ import com.yf.exam.modules.exam.entity.Exam;
 import com.yf.exam.modules.exam.service.ExamService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +46,7 @@ public class ExamController extends BaseController {
     * @param reqDTO
     * @return
     */
+    @RequiresRoles("sa")
     @ApiOperation(value = "添加或修改")
     @RequestMapping(value = "/save", method = { RequestMethod.POST})
     public ApiRest save(@RequestBody ExamSaveReqDTO reqDTO) {
@@ -58,6 +60,7 @@ public class ExamController extends BaseController {
     * @param reqDTO
     * @return
     */
+    @RequiresRoles("sa")
     @ApiOperation(value = "批量删除")
     @RequestMapping(value = "/delete", method = { RequestMethod.POST})
     public ApiRest edit(@RequestBody BaseIdsReqDTO reqDTO) {
@@ -83,6 +86,7 @@ public class ExamController extends BaseController {
      * @param reqDTO
      * @return
      */
+    @RequiresRoles("sa")
     @ApiOperation(value = "查找详情")
     @RequestMapping(value = "/state", method = { RequestMethod.POST})
     public ApiRest state(@RequestBody BaseStateReqDTO reqDTO) {
@@ -117,6 +121,7 @@ public class ExamController extends BaseController {
     * @param reqDTO
     * @return
     */
+    @RequiresRoles("sa")
     @ApiOperation(value = "分页查找")
     @RequestMapping(value = "/paging", method = { RequestMethod.POST})
     public ApiRest<IPage<ExamDTO>> paging(@RequestBody PagingReqDTO<ExamDTO> reqDTO) {
@@ -133,13 +138,12 @@ public class ExamController extends BaseController {
      * @param reqDTO
      * @return
      */
+    @RequiresRoles("sa")
     @ApiOperation(value = "待阅试卷")
     @RequestMapping(value = "/review-paging", method = { RequestMethod.POST})
     public ApiRest<IPage<ExamReviewRespDTO>> reviewPaging(@RequestBody PagingReqDTO<ExamDTO> reqDTO) {
-
         //分页查询并转换
         IPage<ExamReviewRespDTO> page = baseService.reviewPaging(reqDTO);
-
         return super.success(page);
     }
 
